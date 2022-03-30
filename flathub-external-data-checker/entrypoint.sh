b200bdf1ae3522aef4886226c1084cf7a87004d2
Nils Fürniß <contact@nalsai.de>
2022-03-30 16:38:01 +0200

    Add Dockerfile and entrypoint

---
+++ flathub-external-data-checker/entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [[ -z "$GITHUB_WORKSPACE" || -z "$GITHUB_REPOSITORY" ]]; then
+    echo "Script is not running in GitHub Actions CI"
+    exit 1
+fi
+
+git config --global user.name "NalsaiBot" && \
+git config --global user.email "bot@nalsai.de"
+
+mkdir repos
+cd repos
+
+declare -A manifest
+manifest["https://github.com/Nalsai/AegisubFlatpak"]="org.wangqr.Aegisub.yml"
+manifest["https://github.com/Nalsai/MothershipDefender2Flatpak"]="de.Nalsai.MothershipDefender2.yml"
+manifest["https://github.com/Nalsai/SpekFlatpak"]="cc.spek.Spek.yml"
+manifest["https://github.com/DaRealRoyal/TacticalFlatpak"]="com.DaRealRoyal.TacticalMathReturns.yml"
+manifest["https://github.com/Nalsai/gMKVExtractGUIFlatpak"]="net.sourceforge.gMKVExtractGUI.yml"
+manifest["https://github.com/Nalsai/mkv-extractor-qt5-flatpak"]="com.github.mkv-extractor-qt5.yml"
+manifest["https://github.com/Nalsai/summarizer"]="de.haigruppe.summarizer.json"
+
+for m in "${!manifest[@]}"; do
+    git clone --depth 1 $m
+    printf "==> checking %s in %s\n" "${manifest[$m]}" "$m"
+    /app/flatpak-external-data-checker --verbose --update $m/$manifest[$m]
+done
+
+
+
+
+
+
+for repo in ${checker_apps[@]}; do
+    manifest=$(detect_manifest $repo)
+done
